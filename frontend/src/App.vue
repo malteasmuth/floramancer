64afced9e79667702cd130283a466a2a6ad00df7
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-11-21 10:24:54 +0100

    Footer first draft

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <MyNavbar></MyNavbar>
   <router-view></router-view>
-  <div class="placeholders">
-    <h1>Footer placeholder</h1>
-    <p>Site growing</p>
-    <font-awesome-icon icon="fa-solid fa-seedling" />
-    <p>Picture by Kaufmann Mercantile on Unsplash</p>
-  </div>
+  <Footer></Footer>
 </template>
 
 <script>
   import MyNavbar from "./components/MyNavbar.vue";
+  import Footer from "./components/Footer.vue";
 
   export default {
     name: "app",
-    components: { MyNavbar },
+    components: { MyNavbar, Footer },
   };
 
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="footer">
+    <h3>Floramancer - growing <span>green</span></h3>
+    <p>Site growing</p>
+    <font-awesome-icon icon="fa-solid fa-seedling" />
+    <p>Pictures by Kaufmann Mercantile and the Bialons on Unsplash</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Footer",
+  }
+</script>
